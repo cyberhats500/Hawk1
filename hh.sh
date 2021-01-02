d6187a2a674c86730ebf27e84b41e51d57662fba
cyberhats500 <76803733+cyberhats500@users.noreply.github.com>
2021-01-02 16:24:43 +0000

    Add files via upload

---
+++ hh.sh
@@ -0,0 +1,178 @@
+#!/bin/bash
+apt update && apt upgade
+pkg install ruby
+apt-get install ruby
+gem install lolcat
+pkg install cowsay
+pkg install toilet
+apt install git | lolcat
+apt-get install cmatrix | lolcat
+apt install python -y
+apt install toilet | lolcat
+apt update && apt upgade | lolcat
+read -p "Please Enter Your Name: " name
+cowsay -f eyes welcome $name  | lolcat
+ pkg install cmatrix |lolcat
+ clear
+ 
+ echo -e  " \e[35m //////// tools /////////// \e[0m"
+ echo -e "\e[35m ****************************** \e[0m"
+ echo -e "\e[38m ****************************** \e[0m"
+ clear 
+echo -e "\e[36m
+  █  ░░▒▒▒▒▒▒▓▓██                                                                ██▓▓▒▒▒▒▒▒░░  ██
+██      ░░▒▒▒▒▓▓░░                                                            ▓▓▓▓▒▒▒▒░░      ██
+  ██    ░░░░▒▒▒▒▓▓██                                                        ██▓▓▒▒▒▒░░░░    ██
+  ██░░░░  ░░░░▒▒▓▓▓▓██                                                    ██▓▓▓▓▒▒░░░░  ░░░░██
+▓▓▒▒░░░░░░░░░░▒▒▒▒▓▓▓▓▓▓                                                ▓▓▓▓▓▓▒▒▒▒░░░░░░░░░░▒▒██
+██    ░░░░▒▒▒▒░░▒▒▒▒▓▓▓▓▒▒                                            ▒▒▓▓▓▓▒▒▒▒▒▒▒▒▒▒░░░░    ██
+██      ░░▒▒▒▒▒▒▒▒▒▒▒▒▓▓▓▓██                                        ██▓▓▓▓▒▒▒▒▒▒▒▒▒▒▒▒░░      ██
+  ██░░▒▒  ░░▒▒▒▒░░▒▒▒▒▒▒▓▓▓▓██                                    ██▓▓▓▓▒▒▒▒▒▒░░▒▒▒▒░░  ▒▒░░██
+▓▓░░░░░░░░░░░░▒▒▒▒░░▒▒▓▓▒▒▓▓▓▓▒▒                                ▒▒▓▓▓▓▒▒▓▓▒▒░░▒▒▒▒░░░░░░░░░░  ▒▒
+██    ░░░░░░░░░░▒▒▒▒▒▒▒▒▒▒▒▒▓▓▓▓██                            ██▓▓▓▓▒▒▒▒▒▒▒▒▒▒▒▒░░░░░░░░░░    ██
+██      ░░▒▒▒▒▒▒▒▒░░░░▒▒▒▒▒▒▓▓▓▓██                            ██▓▓▓▓▒▒▒▒▒▒░░░░▒▒▒▒▒▒▒▒░░      ██
+  ██    ░░▒▒▒▒▒▒▒▒▒▒▒▒▒▒▓▓▓▓▒▒▓▓▓▓▓▓                        ██▓▓▓▓▒▒▓▓▓▓▒▒▒▒▒▒▒▒▒▒▒▒▒▒░░    ██
+  ██░░░░  ░░▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▓▓██                        ██▓▓▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒░░  ░░░░██
+  ██  ░░▒▒░░░░░░▒▒░░░░▒▒▒▒▒▒▒▒▓▓▓▓▓▓██                    ██▓▓▓▓▓▓▒▒▒▒▒▒▒▒░░░░▒▒░░░░░░▒▒░░  ██
+    ██    ░░▒▒▒▒▒▒▒▒▒▒▒▒▒▒▓▓▓▓▒▒▒▒▓▓██                    ██▓▓▒▒▒▒▓▓▓▓▒▒▒▒▒▒▒▒▒▒▒▒▒▒░░    ▓▓
+    ██    ░░░░▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▓▓▓▓▓▓    ▓▓██▒▒██    ▒▒▓▓▓▓▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒░░░░    ██
+      ██    ░░░░░░▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▓▓▓▓██  ██▒▒▒▒▒▒░░██  ▓▓▓▓▓▓▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒░░░░░░    ██
+      ██  ░░░░░░░░▒▒░░▒▒▒▒░░▒▒▒▒▓▓▒▒▓▓▓▓░░▒▒▓▓▒▒░░██▒▒▓▓░░▓▓▒▒▓▓▒▒▒▒░░▒▒▒▒▒▒▒▒░░░░░░░░  ██
+        ██      ░░░░▒▒▒▒▒▒▒▒▒▒▓▓▓▓▒▒▒▒▓▓██▒▒▒▒████▒▒▒▒██▓▓▒▒▒▒▓▓▓▓▒▒▒▒▒▒▒▒▒▒░░░░      ▓▓
+          ██  ░░░░▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒██▒▒██░░░░██░░██▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒░░░░  ██
+            ██    ░░░░░░░░░░▒▒▒▒░░░░▒▒▒▒▒▒▒▒████████▒▒▒▒▒▒▒▒░░░░▒▒▒▒░░░░░░░░░░    ██
+              ▓▓██  ░░  ░░▒▒▒▒▒▒▒▒▒▒▒▒▒▒░░▒▒▒▒▒▒▒▒▒▒▒▒░░▒▒▒▒▒▒▒▒▒▒▒▒▒▒░░  ░░  ██▓▓
+                  ██  ░░░░░░░░░░░░▒▒▒▒▒▒░░░░▒▒▒▒▒▒▒▒░░░░▒▒▒▒▒▒░░░░░░░░░░░░  ██
+                    ██    ░░  ░░░░░░▒▒░░░░░░░░░░░░░░░░░░░░▒▒░░░░░░  ░░    ██
+                      ▓▓▒▒      ░░░░▒▒▒▒░░░░░░░░░░░░░░░░▒▒▒▒░░░░      ▒▒▓▓
+                          ██████    ░░██▒▒▒▒░░░░░░░░▒▒▒▒██░░    ██████
+                                ████████░░░░▒▒▒▒▒▒▒▒░░░░████████
+                                      ██░░░░░░▒▒▒▒▒▒░░░░██
+                                      ██▒▒░░░░░░░░░░░░▒▒██
+                                        ██▒▒▒▒░░░░▒▒▒▒██
+                                  ▓▓▓▓▓▓██▒▒▒▒▒▒▒▒░░▒▒██▓▓▓▓▓▓
+                                ▓▓░░░░░░██▒▒░░▒▒░░░░▒▒██░░░░░░▓▓
+                                ██░░██░░░░██▒▒░░░░▒▒██░░░░██░░██
+                                ████  ██░░██░░▒▒▒▒░░██░░██  ████
+                                      ██░░██░░░░▒▒░░██░░██
+  \e[95m [*] Made by Hawk Hackers [*] Version 1.0 
+\e[0m"
+echo -e "\e[32m ****************************** \e[0m"
+ echo -e "\e[33m ****************************** \e[0m"
+ echo -e " \e[32m[*]1. banner \e[0m"
+ echo -e " \e[32m[*]2. termuxstyling \e[0m"
+ echo -e " \e[32m[*]3. Ravana (phishing tool) \e[0m"
+ echo -e " \e[32m[*]4. cowsay \e[0m"
+ echo -e " \e[32m[*]5. emailbomb \e[0m"
+ echo -e " \e[32m[*]6. Tool-X \e[0m"
+ echo -e " \e[32m[*]7. Metaploit framework \e[0m"
+ echo -e " \e[32m[*]8. RED_HAWK\e[0m"
+ echo -e " \e[32m[*]9. sqlmap \e[0m"
+ echo -e "\e[32m [*]10. SocialPhish \e[0m"
+ echo -e "\e[32m [*]11. set[social engineering tool] \e[0m"
+ echo -e "\e[32m [*]12. about us \e[0m"
+  
+  echo -e "\e[36m=====================================================\e[0m"
+ read -p " Type Your Choice: " too 
+ #setting case
+ case  $too  in
+ 
+ "1")
+ echo -e "\e[32m banner installing \e[0m" 
+ git clone https://github.com/Bhai4You/Termux-Banner | lolcat
+ cowsay pkg installed " $name "
+ 
+  ;;
+ 
+ 
+ "2")
+ echo -e "\e[32m termuxstyling installing \e[0m" 
+ cd ..
+git clone https://github.com/darkwarrior3/termuxstyling | lolcat
+cowsay pkg installed " $name "
+ ;;
+ 
+ "3")
+ 
+ cowsay pkg installing " $name "
+ echo -e "\e[32m installing \e[0m" 
+ git clone https://github.com/princekrvert/Ravana.git
+ echo "Ravana installed"
+ ;;
+
+ "4")
+ echo -e "\e[32m cowsay \e[0m" 
+pkg install cowsay | lolcat
+cowsay pkg installed " $name "
+ ;;
+ "5" )
+ cowsay pkg installing
+ cd ..
+ git clone https://github.com/princekrvert/gmailbomber.git | lolcat
+ ;;
+ "6")
+ echo -e "\e[32m Tool-X  installing \e[0m" 
+ cd ..
+git clone https://github.com/rajkumardusad/Tool-X | lolcat
+cowsay pkg installed " $name "
+ ;;
+  "7" )
+ cowsay pkg installing
+ cd ..
+ curl -LO https://raw.githubusercontent.com/Hax4us/Metasploit_termux/master/metasploit.sh | lolcat
+ ;;
+ "8")
+ echo -e "\e[32m RED_HAWK installing \e[0m" 
+ cd ..
+git clone https://github.com/Tuhinshubhra/RED_HAWK | lolcat
+cowsay pkg installed " $name "
+ ;;
+ "9")
+ echo -e "\e[32m sqlmap installing \e[0m" 
+ cd ..
+git clone https://github.com/sqlmapproject/sqlmap | lolcat
+cowsay pkg installed " $name "
+ ;;
+ "10")
+ echo -e "\e[32m SocialPhish installing \e[0m" 
+ cd ..
+git clone https://github.com/xHak9x/SocialPhish | lolcat
+cowsay pkg installed " $name "
+ ;;
+ "11")
+ echo -e "\e[32m set[social engineering tool] installing \e[0m" 
+ cd ..
+git clone https://github.com/trustedsec/social-engineer-toolkit | lolcat
+cowsay pkg installed " $name "
+ ;;
+ "12" )
+
+  echo -e "\e[95m
+  _   _                _      _   _            _                 
+| | | | __ ___      _| | __ | | | | __ _  ___| | _____ _ __ ___ 
+| |_| |/ _` \ \ /\ / / |/ / | |_| |/ _` |/ __| |/ / _ \ '__/ __|
+|  _  | (_| |\ V  V /|   <  |  _  | (_| | (__|   <  __/ |  \__ \
+|_| |_|\__,_| \_/\_/ |_|\_\ |_| |_|\__,_|\___|_|\_\___|_|  |___/
+ 
+ 
+\e[0m"
+echo -e "\e[32m [●]Join us"
+echo -e "\6[32m please never Missuse tools,WE ARE NOT RESPONSIBLE FOR YOUR DAMAGE"
+  ;;
+ "*")
+ echo -e "\e[32m wrong keyword \e[0m" 
+ exit
+
+
+ ;;
+
+ esac
+ read -p "WANT TO RUN AGAIN[Y/N]...." run 
+ case $run in
+"Y")
+bash hh.sh
+;;
+"N")
+exit
+;;
+esac
